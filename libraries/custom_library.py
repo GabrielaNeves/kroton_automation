f8aba1861922a1cd8945c9997fb7838db9a55138
Gabriela Neves <gabrielasantosneves11@gmail.com>
2024-10-15 12:53:34 -0300

    refactor: improve the logic for checking the market closing time

--- libraries/custom_library.py
+++ libraries/custom_library.py
@@ -5,17 +5,18 @@ def pegar_preco_fechamento(sigla):
     sigla_yf = yf.Ticker(sigla)
 
     historico = sigla_yf.history(period="5d")
+    print(historico)
 
     data_atual = datetime.now()
     horario_atual = data_atual.time()
 
-    fechamento_mercado = time(17, 0)  # 17:00
+    fechamento_mercado = time(17, 0) 
 
     if horario_atual < fechamento_mercado:
-      
-        if len(historico) > 1:
-            preco_fechamento = historico['Close'].iloc[-2]  
-            return f"{preco_fechamento:.2f}"
+         preco_fechamento = historico['Close'].iloc[-2]  
+         return f"{preco_fechamento:.2f}"
     else:
         preco_fechamento = historico['Close'].iloc[-1] 
         return f"{preco_fechamento:.2f}"
+    
+# print(pegar_preco_fechamento("COGN3.SA"))
